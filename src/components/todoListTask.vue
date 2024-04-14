c7d5ffc19a0faba43e4c03aa331d775b4769b823
YasinDehfuli <ydehfuli@gmail.com>
2024-04-14 18:33:22 +0330

    🚀Update todoListTask.vue add pagination

--- src/components/todoListTask.vue
+++ src/components/todoListTask.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
-import {ref, onMounted} from 'vue';
+import {ref, onMounted, computed} from 'vue';
 import axios from 'axios';
 
 onMounted(() => {
   fetchTodos();
 });
 
+const perPage = 10;
+let id = 0;
+let currentPage = ref(1);
+
+const newTodo = ref('');
+const todoList = ref([]);
+const totalPages = computed(() => Math.ceil(todoList.value.length / perPage));
+
 async function fetchTodos() {
   try {
-    // response get 🔻
     const response = await axios.get('https://jsonplaceholder.typicode.com/todos');
     todoList.value = response.data.slice(0, 200).map((todo) => ({
       id: id++,
@@ -19,32 +26,36 @@ async function fetchTodos() {
   }
 }
 
-
-let id = 0;
-
-const newTodo = ref('');
-const todoList = ref([
-  {
-    id: id++, title: "Sample 1",
-  },
-  {
-    id: id++, title: "Sample 2",
-  },
-  {
-    id: id++, title: "Sample 3",
-  }
-]);
-
 function createTodo() {
-  console.log(newTodo.value)
   todoList.value.push({id: id++, title: newTodo.value});
   newTodo.value = '';
 }
 
 function removeTodo(todo) {
-  todoList.value =  todoList.value.filter((t) => t !== todo);
+  todoList.value = todoList.value.filter((t) => t !== todo);
 }
 
+function goToPage(page) {
+  currentPage.value = page;
+}
+
+function nextPage() {
+  if (currentPage.value < totalPages.value) {
+    currentPage.value++;
+  }
+}
+
+function prevPage() {
+  if (currentPage.value > 1) {
+    currentPage.value--;
+  }
+}
+
+const paginatedTodos = computed(() => {
+  const start = (currentPage.value - 1) * perPage;
+  const end = start + perPage;
+  return todoList.value.slice(start, end);
+});
 
 </script>
 
@@ -55,7 +66,7 @@ function removeTodo(todo) {
   </form>
 
   <ol>
-    <li v-for="todo in todoList" :key="todo.userId">
+    <li v-for="todo in paginatedTodos" :key="todo.id">
       {{ todo.title }}
       <button @click="removeTodo(todo)">
         X
@@ -63,8 +74,29 @@ function removeTodo(todo) {
     </li>
   </ol>
 
+  <div>
+    <button @click="prevPage" :disabled="currentPage === 1">Prev</button>
+    <span>{{ currentPage }} / {{ totalPages }}</span>
+    <button @click="nextPage" :disabled="currentPage === totalPages">Next</button>
+  </div>
 </template>
 
 <style lang="scss" scoped>
+button {
+  border: none;
+  border-radius: 5px;
+  padding: 0.2em;
+  margin: 7px;
+}
 
+li {
+  line-height: 20px;
+}
+
+input {
+  border: none;
+  background: #e9e9ed;
+  padding: 3.5px;
+  border-radius: 5px;
+}
 </style>
