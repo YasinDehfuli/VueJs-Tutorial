7e0485896b04af4e8e7dced916da7714f0cef265
YasinDehfuli <ydehfuli@gmail.com>
2024-04-14 12:05:09 +0330

    Add onMounted component!

---
+++ src/components/LifecyclAndTemplateRefs.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import {ref ,onMounted} from "vue";
+
+const pElementOnMount = ref(null);
+onMounted(() => {
+  pElementOnMount.value.textContent = 'Text Mounted!!!!!';
+})
+</script>
+
+<template>
+  <p ref="pElementOnMount">
+    Hello World
+  </p>
+</template>
