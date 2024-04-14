9c3b74449566e500f0ca74bdb202a8d5c2cf34cc
YasinDehfuli <ydehfuli@gmail.com>
2024-04-14 17:38:22 +0330

    📥 import todoListTask.vue component

--- src/App.vue
+++ src/App.vue
@@ -8,29 +8,32 @@ import ConditionalRendering from "./components/ConditionalRendering.vue";
 import ListRendering from "./components/ListRendering.vue";
 import ComputedProperty from "./components/ComputedProperty.vue";
 import LifecyclAndTemplateRefs from "./components/LifecyclAndTemplateRefs.vue";
+import TodoListTask from "./components/todoListTask.vue";
+import Todo from "./components/todo.vue";
 </script>
 
 <template>
-  <h1>
-    HELLO WORLD
-  </h1>
 
-  <hr>
-  <AppRefReactive></AppRefReactive>
-  <hr>
-  <DynamicGreen></DynamicGreen>
-  <hr>
-  <num-count></num-count>
-  <hr>
-  <input-bind></input-bind>
-  <hr>
-  <ConditionalRendering></ConditionalRendering>
-  <hr>
-  <list-rendering></list-rendering>
-  <hr>
-  <computed-property></computed-property>
-  <hr>
-  <lifecycl-and-template-refs></lifecycl-and-template-refs>
+<!--  <hr>-->
+<!--  <AppRefReactive></AppRefReactive>-->
+<!--  <hr>-->
+<!--  <DynamicGreen></DynamicGreen>-->
+<!--  <hr>-->
+<!--  <num-count></num-count>-->
+<!--  <hr>-->
+<!--  <input-bind></input-bind>-->
+<!--  <hr>-->
+<!--  <ConditionalRendering></ConditionalRendering>-->
+<!--  <hr>-->
+<!--  <list-rendering></list-rendering>-->
+<!--  <hr>-->
+<!--  <computed-property></computed-property>-->
+<!--  <hr>-->
+<!--  <lifecycl-and-template-refs></lifecycl-and-template-refs>-->
+
+  <todo-list-task></todo-list-task>
+
+<!--  <todo></todo>-->
 
 
 </template>
