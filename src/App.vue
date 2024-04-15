0e1ed4daac60c906221fa7fe1b509f64fd1ff341
YasinDehfuli <ydehfuli@gmail.com>
2024-04-15 11:57:32 +0330

    import todoListTask.vue in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,40 @@
 <script setup lang="ts">
-
-import AppRefReactive from "./components/AppRefReactive.vue";
-import DynamicGreen from "./components/DynamicGreen.vue";
-import NumCount from "./components/NumCount.vue";
-import InputBind from "./components/InputBind.vue";
-import ConditionalRendering from "./components/ConditionalRendering.vue";
-import ListRendering from "./components/ListRendering.vue";
-import ComputedProperty from "./components/ComputedProperty.vue";
-import LifecyclAndTemplateRefs from "./components/LifecyclAndTemplateRefs.vue";
+// import AppRefReactive from "./components/AppRefReactive.vue";
+// import DynamicGreen from "./components/DynamicGreen.vue";
+// import NumCount from "./components/NumCount.vue";
+// import InputBind from "./components/InputBind.vue";
+// import ConditionalRendering from "./components/ConditionalRendering.vue";
+// import ListRendering from "./components/ListRendering.vue";
+// import ComputedProperty from "./components/ComputedProperty.vue";
+// import LifecyclAndTemplateRefs from "./components/LifecyclAndTemplateRefs.vue";
+// import TodoListTask from "./components/todoListTask.vue";
+// import InputIrt from "./components/input-irt.vue";
 import TodoListTask from "./components/todoListTask.vue";
-import Todo from "./components/todo.vue";
 </script>
 
 <template>
-
-<!--  <hr>-->
-<!--  <AppRefReactive></AppRefReactive>-->
-<!--  <hr>-->
-<!--  <DynamicGreen></DynamicGreen>-->
-<!--  <hr>-->
-<!--  <num-count></num-count>-->
-<!--  <hr>-->
-<!--  <input-bind></input-bind>-->
-<!--  <hr>-->
-<!--  <ConditionalRendering></ConditionalRendering>-->
-<!--  <hr>-->
-<!--  <list-rendering></list-rendering>-->
-<!--  <hr>-->
-<!--  <computed-property></computed-property>-->
-<!--  <hr>-->
-<!--  <lifecycl-and-template-refs></lifecycl-and-template-refs>-->
-
+  <!--  <hr>-->
+  <!--  <AppRefReactive></AppRefReactive>-->
+  <!--  <hr>-->
+  <!--  <DynamicGreen></DynamicGreen>-->
+  <!--  <hr>-->
+  <!--  <num-count></num-count>-->
+  <!--  <hr>-->
+  <!--  <input-bind></input-bind>-->
+  <!--  <hr>-->
+  <!--  <ConditionalRendering></ConditionalRendering>-->
+  <!--  <hr>-->
+  <!--    <list-rendering></list-rendering>-->
+  <!--  <hr>-->
+  <!--  <computed-property></computed-property>-->
+  <!--  <hr>-->
+  <!--    <lifecycl-and-template-refs></lifecycl-and-template-refs>-->
+  <!--  <input-irt></input-irt>-->
   <todo-list-task></todo-list-task>
-
-<!--  <todo></todo>-->
-
-
 </template>
 
 <style scoped>
+
+
 
 </style>
